329c1bd5d98225fad1016149d8816c0d2411ea29
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 00:48:33 +0530

    Add project files.

---
+++ AngularJsDemonstration_6/Scripts/appScripts.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module('testpp', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider.when('/login', {
+        templateUrl: 'Pages/login.html',
+        controller: 'loginController'
+    }).when('/register', {
+        templateUrl: 'Pages/registration.html',
+        controller: 'registrationController'
+    }).when('/contact', {
+        templateUrl: 'Pages/contactUs.html',
+        controller: 'contactUsController'
+    }).otherwise({
+        redirectTo:'login'
+    });
+
+}]);
---
+++ AngularJsDemonstration_6/Scripts/controllerScripts.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="angular.js" />
+
+app.controller('loginController', function ($scope) {
+
+    $scope.onLogIn = function () {
+        alert('User has Clicked on LogIn Button');
+        alert(JSON.stringify($scope.loginInfo));
+    };
+
+});
+
+app.controller('registrationController', function ($scope) {
+    $scope.onRegister = function () {
+        alert('User has Clicked on Register Button');
+        alert(JSON.stringify($scope.userInfo));
+    };
+});
+
+app.controller('contactUsController', function ($scope) {
+
+});
